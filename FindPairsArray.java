5ccb61dfbd17e0a7c1e1ce677555c5c8caa406aa
Karmakarchandan2000 <92676110+Karmakarchandan2000@users.noreply.github.com>
2021-12-10 21:20:49 +0530

    Q1.  FindPairsArray
    Q1. Write a program to find all pairs of an integer array whose sum is equal to a given number?

---
+++ FindPairsArray.java
@@ -0,0 +1,25 @@
+
+package com.DSAssignment;
+
+public class  FindPairsArray {
+public static void main(String args[])
+{
+int[] arr = { 2,6,8,-5,-2,13};
+int sum = 8;
+getPairsCount(arr, sum);
+}
+
+public static void getPairsCount(int[] arr, int sum)
+{
+
+int count = 0;
+for (int i = 0; i < arr.length; i++)
+for (int j = i + 1; j < arr.length; j++)
+if ((arr[i] + arr[j]) == sum)
+{
+System.out.println("Pairs "+ arr[i] +" "+ arr[j]);
+count++;
+}
+System.out.println("Count of pairs is %d"+ count);
+}
+}
